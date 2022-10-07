87043f09d8f505689c67d4e876f9e1831cbe24ce
luketebo <mianqiangyang@gmail.com>
2022-10-07 22:15:30 +0800

    pycharm 我 艹 你 🐎

--- pythonProject/DBSCAN/dbscan.py
+++ pythonProject/DBSCAN/dbscan.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 samples = np.loadtxt("kmeansSamples.txt")
 clustering = DBSCAN(eps=5, min_samples=5).fit(samples)
-x = clustering.labels
+x = clustering.labels_
 print(x)
-plt.scatter(samples[:,0], samples[:, 1], c=clustering.labels_ + 1.5, linewidths=np.power(clustering.labels_ + 1.5, 2))
+plt.scatter(samples[:, 0], samples[:, 1], c=clustering.labels_ + 1.5, linewidths=np.power(clustering.labels_ + 1.5, 2))
 plt.show()
