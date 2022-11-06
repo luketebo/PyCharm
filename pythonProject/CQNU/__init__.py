2a9b3b26771f285d0964f08d6a3e457c301a8729
luketebo <mianqiangyang@gmail.com>
2022-11-06 13:00:51 +0800

    i can not do this work

--- pythonProject/CQNU/__init__.py
+++ pythonProject/CQNU/__init__.py
@@ -6,6 +6,7 @@ class Login(ttk.Toplevel):
     """
     Login 弹窗
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Login")
@@ -46,19 +47,103 @@ class CqnuTool(ttk.Frame):
         buttonBar.pack(fill=X, pady=1, side=TOP)
 
         # DreamRoom btn
-        def dreamRoom():
-            login = Login()
+        def dreamRoom(root):
+            LoginFrame = ttk.Frame(root)
+            LoginFrame.pack(pady=200, padx=300)
+            # 学号 密码
+            LoginFrame_1 = ttk.Frame(
+                master=LoginFrame
+            )
+            LoginFrame_1.pack()
+
+            label = ttk.Label(
+                master=LoginFrame_1, text="学号： ",
+            )
+            label.grid(row=0, column=0, padx=15, pady=15)
+
+            entry = ttk.Entry(
+                master=LoginFrame_1
+            )
+            entry.grid(row=0, column=1)
+
+            label = ttk.Label(
+                master=LoginFrame_1, text="密码： "
+            )
+            label.grid(row=1, column=0)
+
+            entry = ttk.Entry(
+                master=LoginFrame_1
+            )
+            entry.grid(row=1, column=1)
+            # 验证码
+            LoginFrame_1 = ttk.Frame(
+                master=LoginFrame
+            )
+            LoginFrame_1.pack()
+
+            label = ttk.Label(
+                master=LoginFrame_1, text="验证码"
+            )
+            label.grid(row=0, column=0, pady=15, padx=15, ipadx=35)
+            label = ttk.Label(
+                master=LoginFrame_1, text='图片'
+            )
+            label.grid(row=0, column=1, pady=15, padx=15, ipadx=35)
+            # 登录按钮
+            LoginFrame_1 = ttk.Frame(
+                master=LoginFrame
+            )
+            LoginFrame_1.pack()
+
+            def DreamRoom(frameRoot):
+                print(type(frameRoot))
+                frameRoot.distroy()
+                pass
+
+            btn = ttk.Button(
+                master=LoginFrame_1, text="Login",
+                command=lambda: DreamRoom(self)
+            )
+            btn.pack(pady=15, ipadx=105)
+
+            # 多选
+            LoginFrame_1 = ttk.Frame(
+                master=LoginFrame
+            )
+            LoginFrame_1.pack()
+
+            che = ttk.Checkbutton(
+                master=LoginFrame_1, text="记住密码"
+            )
+            che.grid(row=0, column=0, pady=15, padx=45)
+            che = ttk.Checkbutton(
+                master=LoginFrame_1, text="自动登录"
+            )
+            che.grid(row=0, column=1, pady=15, padx=45)
+            che = ttk.Checkbutton(
+                master=LoginFrame_1, text="记住密码"
+            )
+            che.grid(row=1, column=0)
+            che = ttk.Checkbutton(
+                master=LoginFrame_1, text="记住密码"
+            )
+            che.grid(row=1, column=1)
+
+            # login = Login()
             pass
+
         btn = ttk.Button(
             master=buttonBar, text="DreamRoom",
             compound=LEFT,
-            command=dreamRoom()
+            # command=lambda: dreamRoom(self)
+            command=dreamRoom(self)
         )
         btn.pack(side=LEFT, ipadx=5, ipady=5, padx=(1, 0), pady=1)
 
         # Sports btn
         def sportBook():
             pass
+
         btn = ttk.Button(
             master=buttonBar, text="SportBook",
             compound=LEFT,
@@ -73,6 +158,6 @@ class CqnuTool(ttk.Frame):
 
 
 if __name__ == "__main__":
-    app = ttk.Window(title="CNU Tools", size=[1200, 800])
+    app = ttk.Window(title="CNU Tools", size=[1200, 800], resizable=[False, False])
     cqnu = CqnuTool(app)
     app.mainloop()
