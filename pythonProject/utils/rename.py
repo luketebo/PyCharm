dc2bec28d79239768c98c3134fcb97b5d60cc55a
luketebo <mianqiangyang@gmail.com>
2022-12-10 21:06:38 +0800

    submit the demo

--- pythonProject/utils/rename.py
+++ pythonProject/utils/rename.py
@@ -6,36 +6,20 @@ import os
 """
 
 
-# path = input('文件路劲：')
-# path = 'D:\Desktop\截图'
-
-# fileList = os.listdir(path)
-
-# n = 0
-
-
-# for i in fileList:
-#     oldName = path + os.sep + fileList[n]
-#
-#     newName = path + os.sep + str(n+1) + '.png'
-#     print(oldName + '==========>' + newName)
-#     # os.rename(oldName, newName)
-#
-#     n += 1
-
 def ReName(path, number):
     fileList = os.listdir(path)
     f = 0
     for item in fileList:
         oldName = path + os.sep + fileList[f]
-        newName = path + os.sep + str(number + 1 + f) + '.png'
+        newName = path + os.sep + str(number + 1 + f) + '.jpeg'
         # print(newName)
         os.rename(oldName, newName)
+        print(oldName + '=========>' + newName + '\n')
         f += 1
 
 
 if __name__ == '__main__':
-    p = 'D:\Desktop\截图'
-    n = 30
+    p = "D:\Desktop\照片"
+    n = 76
     ReName(p, n)
     pass
